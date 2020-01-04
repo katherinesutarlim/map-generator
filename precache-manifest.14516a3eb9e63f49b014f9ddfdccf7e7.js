8c8f0c10e2558d21008c912ccdf788cf12690094
Katherine Sutarlim <katherine.rnslim.sutarlim@gmail.com>
2020-01-04 23:49:07 +1100

    Updates

---
+++ precache-manifest.14516a3eb9e63f49b014f9ddfdccf7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be708d1cf84a685951055da32ee2ee2",
+    "url": "/map-generator/index.html"
+  },
+  {
+    "revision": "2c3408dad4e84dcdf214",
+    "url": "/map-generator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31ce4587b238160918b2",
+    "url": "/map-generator/static/js/2.f72eb410.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/map-generator/static/js/2.f72eb410.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c3408dad4e84dcdf214",
+    "url": "/map-generator/static/js/main.cfd1da62.chunk.js"
+  },
+  {
+    "revision": "6fdbabc29de6e7de3a0f",
+    "url": "/map-generator/static/js/runtime-main.7aca7cf1.js"
+  }
+]);
